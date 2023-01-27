3238c1710c9fd3e2531e009499639b9f31f4e036
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-01-27 10:10:33 -0800

    add functionality to uncheck the topping

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ Pizza.prototype.PizzaPrice = function () {
   pizzaPrice.push(pizzaPriceSize);
   pizzaPrice.push(pizzaPriceToppings);
   return pizzaPrice;
-}; 
+};
 
 
 let pizzaOrder = new Pizza(0, []);
@@ -31,7 +31,7 @@ function smallSize(event) {
   return price[0];
 }
 function mediumSize(event) {
-  priceSize = 20; 
+  priceSize = 20;
   document.getElementById("medium").src = "img/pizzachecked.jpg";
   document.getElementById("small").src = "img/pizza.jpg";
   document.getElementById("large").src = "img/pizza.jpg";
@@ -40,7 +40,7 @@ function mediumSize(event) {
   return price[0];
 }
 function largeSize(event) {
-  priceSize = 25; 
+  priceSize = 25;
   document.getElementById("large").src = "img/pizzachecked.jpg";
   document.getElementById("small").src = "img/pizza.jpg";
   document.getElementById("medium").src = "img/pizza.jpg";
@@ -49,25 +49,66 @@ function largeSize(event) {
   return price[0];
 }
 
+let hamTimesClicked = 0;
 function ham(event) {
-  priceToppings = price[1] + 2; 
-  document.getElementById("ham").src = "img/checkedham.jpg";
+  hamTimesClicked += 1;
+  console.log(hamTimesClicked);
+  if (hamTimesClicked % 2 == 0) {
+    priceToppings = price[1] - 2;
+    document.getElementById("ham").src = "img/ham.jpg";
+  }
+  else {
+    priceToppings = price[1] + 2;
+    document.getElementById("ham").src = "img/checkedham.jpg";
+  }
   console.log(priceToppings);
   price[1] = priceToppings;
   return price[1];
 }
 
+let salamiTimesClicked = 0;
 function salami(event) {
-  priceToppings = price[1] + 2; 
-  document.getElementById("salami").src = "img/checkedSalami.jpg";
+  salamiTimesClicked += 1;
+  if (salamiTimesClicked % 2 == 0) {
+    priceToppings = price[1] - 2;
+    document.getElementById("salami").src = "img/salami.jpg";
+  }
+  else {
+    priceToppings = price[1] + 2;
+    document.getElementById("salami").src = "img/checkedSalami.jpg";
+  }
   console.log(priceToppings);
   price[1] = priceToppings;
   return price[1];
 }
 
+let cheeseTimesClicked = 0;
 function cheese(event) {
-  priceToppings = price[1] + 2; 
-  document.getElementById("cheese").src = "img/extraCheese.jpg";
+  cheeseTimesClicked += 1;
+  if (cheeseTimesClicked % 2 == 0) {
+    priceToppings = price[1] - 2;
+    document.getElementById("cheese").src = "img/cheese.jpg";
+  }
+  else {
+    priceToppings = price[1] + 2;
+    document.getElementById("cheese").src = "img/extraCheese.jpg";
+  }
+  console.log(priceToppings);
+  price[1] = priceToppings;
+  return price[1];
+}
+
+let mushroomsTimesClicked = 0;
+function mushrooms(event) {
+  priceToppings = price[1] + 2;
+  mushroomsTimesClicked += 1;
+  if (mushroomsTimesClicked % 2 == 0) {
+    priceToppings = price[1] - 2;
+    document.getElementById("mushrooms").src = "img/mushrooms.jpg";
+  }
+  else {
+    document.getElementById("mushrooms").src = "img/mushroomsChecked.jpg";
+  }
   console.log(priceToppings);
   price[1] = priceToppings;
   return price[1];
@@ -78,10 +119,11 @@ function cheese(event) {
 
 window.addEventListener("load", function () {
   console.log(document.getElementById("small"));
-document.getElementById("small").addEventListener("click", smallSize);
-document.getElementById("medium").addEventListener("click", mediumSize);
-document.getElementById("large").addEventListener("click", largeSize);
-document.getElementById("ham").addEventListener("click", ham);
-document.getElementById("salami").addEventListener("click", salami);
-document.getElementById("cheese").addEventListener("click", cheese);
+  document.getElementById("small").addEventListener("click", smallSize);
+  document.getElementById("medium").addEventListener("click", mediumSize);
+  document.getElementById("large").addEventListener("click", largeSize);
+  document.getElementById("ham").addEventListener("click", ham);
+  document.getElementById("salami").addEventListener("click", salami);
+  document.getElementById("cheese").addEventListener("click", cheese);
+  document.getElementById("mushrooms").addEventListener("click", mushrooms);
 });
