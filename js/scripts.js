ec387f394dedf64a208cb3dd48c8460ecb948bdb
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-01-27 09:08:19 -0800

    add event listeners for pictures

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,41 @@
 
 function Pizza(size, toppings) {
   this.size = size;
-  this.toppings = topings;
+  this.toppings = toppings;
 }
+
+Pizza.prototype.PizzaPrice = function () {
+  let pizzaPrice = 15;
+  return pizzaPrice;
+}; 
+
+
+let pizzaOrder = new Pizza(0, []);
+console.log(pizzaOrder);
+let pizzaPrice = pizzaOrder.PizzaPrice();
+console.log(pizzaPrice);
+
+//User Interface Logic
+function smallSize(pizzaPrice, event) {
+  pizzaPrice = pizzaPrice;
+  console.log(pizzaPrice);
+}
+function mediumSize(pizzaPrice, event) {
+  pizzaPrice = pizzaPrice + 5; 
+  console.log(pizzaPrice);
+}
+function largeSize(pizzaPrice, event) {
+  pizzaPrice = pizzaPrice + 10; 
+  console.log(pizzaPrice);
+}
+
+
+
+
+window.addEventListener("load", function () {
+  console.log(document.getElementById("small"));
+document.getElementById("small").addEventListener("click", smallSize);
+document.getElementById("medium").addEventListener("click", mediumSize);
+document.getElementById("large").addEventListener("click", largeSize);
+
+});
